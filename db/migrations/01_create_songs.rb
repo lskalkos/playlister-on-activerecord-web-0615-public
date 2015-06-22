dde3b594879a0565a91b7f98b36dcae2507e3762
Lia Skalkos <hello@liaskalkos.net>
2015-06-22 13:00:19 -0400

    Complete

--- db/migrations/01_create_songs.rb
+++ db/migrations/01_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.belongs_to :artist, index: true
+      t.belongs_to :genre, index: true
+      t.string :name
+    end
+  end
 end
